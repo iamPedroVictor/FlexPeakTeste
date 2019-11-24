1f50595412e18fb6b678adf67c2436a31d7057b7
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-11-24 03:06:52 -0400

    Criado as rotas de login e registro em api/.

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::namespace('Api')->name('api.')->group(function(){
+    Route::post('login', 'VendedorController@login');
+    Route::post('register', 'VendedorController@register');
+});
