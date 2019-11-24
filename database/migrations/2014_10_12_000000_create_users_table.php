a3e06f89906edfe5b26585950dc12627cd09d426
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-11-24 02:29:36 -0400

    Adicionado campos para a tabela Usuarios.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
+        Schema::create('Usuarios', function (Blueprint $table) {
+            $table->bigIncrements('id_usuario');
+            $table->string('nome');
+            $table->string('password');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('cep');
+            $table->string('rua');
             $table->rememberToken();
             $table->timestamps();
         });
