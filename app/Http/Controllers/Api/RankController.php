6bc3fceb0e550c5e1eb729ded8cd3185b7302ce0
Pedro Victor Ferreira <pedro.victor.ferreira@outlook.com.br>
2019-11-26 08:42:59 -0400

    Rank

--- app/Http/Controllers/Api/RankController.php
+++ app/Http/Controllers/Api/RankController.php
@@ -17,10 +17,10 @@ class RankController extends Controller
         }
         if(!$request->has('mes') && !$request->has('ano')){
             $produtosId = DB::table('vendas')
-                ->selectRaw(DB::raw('SUM("quantidade"), "produto" group by produto order by SUM("quantidade") desc limit ?', [$limit]))->get();
+                ->selectRaw('SUM(quantidade), produto group by produto order by SUM(quantidade) desc limit ?', [$limit])->get();
         }else{
             $produtosId = DB::table('vendas')
-                ->selectRaw(DB::raw('SUM("quantidade"), "produto" group by produto order by SUM("quantidade") desc limit ?', [$limit]))
+                ->selectRaw('SUM(quantidade), produto group by produto order by SUM(quantidade) desc limit ?', [$limit])
                 ->whereMonth('created_at', $request->mes)
                 ->whereYear('created_at', $request->ano)
                 ->get();
